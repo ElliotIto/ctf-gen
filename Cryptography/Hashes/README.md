364eca3aaa03a503967ced9cbfb85acd9aafb637
Elliot Ito <elliot4@hawaii.edu>
2018-03-02 16:12:15 -1000

    Updated hash-gen.sh to working status and updated README.md to show how to use

---
+++ Cryptography/Hashes/README.md
@@ -0,0 +1,13 @@
+Hashes 
+----
+
+## hashgen.sh
+
+Creates hashlist from lines in provided file. 
+
+Requires that the appropiate <hash>sum is on the Linux distribution (ie. md5sum, sha256sum).
+
+'''
+./hashgen.sh <HASH> <INPUT FILE> <OUTPUT FILE>
+./hashgen.sh md5 test_list.txt md5_list.txt
+'''
--- Cryptography/Hashes/hash-gen.sh
+++ Cryptography/Hashes/hash-gen.sh
@@ -1,8 +1,9 @@
 #/bin/bash
-$hash = $1
-$file = $2
-$output = $3
-
-for
-echo -n "$line" | md5sum >> $output
-
+hash="$1sum"
+file="$2"
+output="$3"
+hashsum="$(echo $hash | tr '[:upper:]' '[:lower:]')"
+echo "Outputting $hashsum of lines in $file to $output" 
+while read line; do
+	echo -n "$line" | $hashsum | awk '{print $1}' >> "$output"
+done <$file
